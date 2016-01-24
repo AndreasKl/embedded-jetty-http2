cfaa853eaf798f3f965070de5c0d8df98b8e0461
Andreas Kluth <mail@andreaskluth.net>
2016-01-24 20:56:21 +0100

    Back to spaces.

--- src/main/java/net/andreaskluth/Application.java
+++ src/main/java/net/andreaskluth/Application.java
@@ -32,128 +32,128 @@ import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class Application {
 
-	private static final Logger LOG = Log.getLogger(Application.class);
+    private static final Logger LOG = Log.getLogger(Application.class);
 
-	public static void main(String[] args) throws Exception {
-		try (JettyStarter starter = new JettyStarter();) {
-			starter.start();
-			LOG.info("Press any key to stop Jetty.");
-			System.in.read();
-		}
-	}
+    public static void main(String[] args) throws Exception {
+        try (JettyStarter starter = new JettyStarter();) {
+            starter.start();
+            LOG.info("Press any key to stop Jetty.");
+            System.in.read();
+        }
+    }
 
-	public static class NoopServlet extends HttpServlet {
+    public static class NoopServlet extends HttpServlet {
 
-		private static final long serialVersionUID = 1L;
+        private static final long serialVersionUID = 1L;
 
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-				throws ServletException, IOException {
-			resp.setContentType("text/html;charset=UTF-8");
-			resp.getWriter().append("OK+");
-		}
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+                throws ServletException, IOException {
+            resp.setContentType("text/html;charset=UTF-8");
+            resp.getWriter().append("OK+");
+        }
 
-	}
+    }
 
-	private static class JettyStarter implements AutoCloseable {
+    private static class JettyStarter implements AutoCloseable {
 
-		private static final int SSL_PORT = 8443;
-		private final Server server;
-		private final HttpConfiguration config = createHttpConfiguration();
+        private static final int SSL_PORT = 8443;
+        private final Server server;
+        private final HttpConfiguration config = createHttpConfiguration();
 
-		public JettyStarter() {
-			HttpConnectionFactory httpFactory = new HttpConnectionFactory(config);
-			HTTP2ServerConnectionFactory http2Factory = new HTTP2ServerConnectionFactory(
-					config);
-			ALPNServerConnectionFactory alpn = createAlpnProtocolFactory(httpFactory);
-			Server server = createServer(httpFactory, http2Factory, alpn);
+        public JettyStarter() {
+            HttpConnectionFactory httpFactory = new HttpConnectionFactory(config);
+            HTTP2ServerConnectionFactory http2Factory = new HTTP2ServerConnectionFactory(
+                    config);
+            ALPNServerConnectionFactory alpn = createAlpnProtocolFactory(httpFactory);
+            Server server = createServer(httpFactory, http2Factory, alpn);
 
-			HandlerWrapper servletHandler = createServletHandlerWithServlet();
-			HandlerWrapper gzipHandler = createGzipHandler();
-			gzipHandler.setHandler(servletHandler);
-			server.setHandler(gzipHandler);
+            HandlerWrapper servletHandler = createServletHandlerWithServlet();
+            HandlerWrapper gzipHandler = createGzipHandler();
+            gzipHandler.setHandler(servletHandler);
+            server.setHandler(gzipHandler);
 
-			this.server = server;
-		}
+            this.server = server;
+        }
 
-		private Server createServer(HttpConnectionFactory httpConnectionFactory,
-				HTTP2ServerConnectionFactory http2ConnectionFactory,
-				ALPNServerConnectionFactory alpn) {
-			Server server = new Server();
-			server.setRequestLog(new AsyncNCSARequestLog());
+        private Server createServer(HttpConnectionFactory httpConnectionFactory,
+                HTTP2ServerConnectionFactory http2ConnectionFactory,
+                ALPNServerConnectionFactory alpn) {
+            Server server = new Server();
+            server.setRequestLog(new AsyncNCSARequestLog());
 
-			ServerConnector connector = new ServerConnector(server, prepareSsl(alpn),
-					alpn, http2ConnectionFactory, httpConnectionFactory);
-			connector.setPort(SSL_PORT);
-			server.addConnector(connector);
+            ServerConnector connector = new ServerConnector(server, prepareSsl(alpn),
+                    alpn, http2ConnectionFactory, httpConnectionFactory);
+            connector.setPort(SSL_PORT);
+            server.addConnector(connector);
 
-			return server;
-		}
+            return server;
+        }
 
-		private GzipHandler createGzipHandler() {
-			GzipHandler gzipHandler = new GzipHandler();
-			gzipHandler.setIncludedPaths("/*");
-			gzipHandler.setMinGzipSize(0);
-			gzipHandler.setIncludedMimeTypes("text/plain", "text/html");
-			return gzipHandler;
-		}
+        private GzipHandler createGzipHandler() {
+            GzipHandler gzipHandler = new GzipHandler();
+            gzipHandler.setIncludedPaths("/*");
+            gzipHandler.setMinGzipSize(0);
+            gzipHandler.setIncludedMimeTypes("text/plain", "text/html");
+            return gzipHandler;
+        }
 
-		private ServletContextHandler createServletHandlerWithServlet() {
-			ServletContextHandler context = new ServletContextHandler(
-					ServletContextHandler.SESSIONS);
+        private ServletContextHandler createServletHandlerWithServlet() {
+            ServletContextHandler context = new ServletContextHandler(
+                    ServletContextHandler.SESSIONS);
 
-			FilterHolder pushCacheFilter = context.addFilter(PushCacheFilter.class, "/*",
-					null);
-			Map<String, String> config = new HashMap<>();
-			config.put("maxAssociations", "32");
-			config.put("ports", Objects.toString(SSL_PORT));
-			pushCacheFilter.setInitParameters(config);
+            FilterHolder pushCacheFilter = context.addFilter(PushCacheFilter.class, "/*",
+                    null);
+            Map<String, String> config = new HashMap<>();
+            config.put("maxAssociations", "32");
+            config.put("ports", Objects.toString(SSL_PORT));
+            pushCacheFilter.setInitParameters(config);
 
-			context.addServlet(NoopServlet.class, "/*");
-			context.setContextPath("/");
+            context.addServlet(NoopServlet.class, "/*");
+            context.setContextPath("/");
 
-			return context;
-		}
+            return context;
+        }
 
-		private ALPNServerConnectionFactory createAlpnProtocolFactory(
-				HttpConnectionFactory httpConnectionFactory) {
-			NegotiatingServerConnectionFactory.checkProtocolNegotiationAvailable();
-			ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
-			alpn.setDefaultProtocol(httpConnectionFactory.getProtocol());
-			return alpn;
-		}
+        private ALPNServerConnectionFactory createAlpnProtocolFactory(
+                HttpConnectionFactory httpConnectionFactory) {
+            NegotiatingServerConnectionFactory.checkProtocolNegotiationAvailable();
+            ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+            alpn.setDefaultProtocol(httpConnectionFactory.getProtocol());
+            return alpn;
+        }
 
-		private SslConnectionFactory prepareSsl(ALPNServerConnectionFactory alpn) {
-			SslContextFactory sslContextFactory = new SslContextFactory();
-			sslContextFactory.setKeyStorePath(
-					Application.class.getResource("/keystore").toExternalForm());
-			sslContextFactory.setKeyStorePassword("changeit");
-			sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
-			sslContextFactory.setUseCipherSuitesOrder(true);
-			SslConnectionFactory ssl = new SslConnectionFactory(sslContextFactory,
-					alpn.getProtocol());
-			return ssl;
-		}
+        private SslConnectionFactory prepareSsl(ALPNServerConnectionFactory alpn) {
+            SslContextFactory sslContextFactory = new SslContextFactory();
+            sslContextFactory.setKeyStorePath(
+                    Application.class.getResource("/keystore").toExternalForm());
+            sslContextFactory.setKeyStorePassword("changeit");
+            sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
+            sslContextFactory.setUseCipherSuitesOrder(true);
+            SslConnectionFactory ssl = new SslConnectionFactory(sslContextFactory,
+                    alpn.getProtocol());
+            return ssl;
+        }
 
-		private static HttpConfiguration createHttpConfiguration() {
-			HttpConfiguration config = new HttpConfiguration();
-			config.setSecureScheme("https");
-			config.setSecurePort(SSL_PORT);
-			config.setSendXPoweredBy(false);
-			config.setSendServerVersion(false);
-			config.addCustomizer(new SecureRequestCustomizer());
-			return config;
-		}
+        private static HttpConfiguration createHttpConfiguration() {
+            HttpConfiguration config = new HttpConfiguration();
+            config.setSecureScheme("https");
+            config.setSecurePort(SSL_PORT);
+            config.setSendXPoweredBy(false);
+            config.setSendServerVersion(false);
+            config.addCustomizer(new SecureRequestCustomizer());
+            return config;
+        }
 
-		public void start() throws Exception {
-			server.start();
-		}
+        public void start() throws Exception {
+            server.start();
+        }
 
-		@Override
-		public void close() throws Exception {
-			server.stop();
-		}
+        @Override
+        public void close() throws Exception {
+            server.stop();
+        }
 
-	}
+    }
 
 }
